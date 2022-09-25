a70f2a7b1e3f9d823133cf21448c161fea64531e
Vinicius Birsenek <vinicius@t2s.com.br>
2022-09-24 22:47:12 -0300

    first commit

---
+++ PortCrawler-API/Controllers/PortCrawlerController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PortCrawler_API.Interfaces;
+
+namespace PortCrawler_API.Controllers
+{
+    [Route("Port")]
+    [ApiController]
+    public class PortCrawlerController : ControllerBase
+    {
+        private IPortCrawler _portCrawler;
+
+        public PortCrawlerController(IPortCrawler portCrawler)
+        {
+            _portCrawler = portCrawler;
+        }
+
+        [HttpGet(Name = "GetPortNews")]
+        public async Task<string> GetPortNews()
+        {
+           var response = await _portCrawler.StartPortCrawler();
+
+            return response;
+        }
+    }
+
+   
+}
---
+++ PortCrawler-API/Services/PortCrawlerService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PortCrawler_API.Interfaces;
+using System.Net;
+
+namespace PortCrawler_API.Services
+{
+    public class PortCrawlerService : IPortCrawler
+    {
+        //public async Task<string> StartPortCrawler()
+        //{
+        //    string fullUrl = "https://news.ycombinator.com";
+        //    var response = CallUrl(fullUrl).Result;
+        //    return response;
+        //}
+
+        public async Task<string> StartPortCrawler()
+        {
+            string fullUrl = "https://news.ycombinator.com/";
+            HttpClient client = new HttpClient();
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls13;
+            client.DefaultRequestHeaders.Accept.Clear();
+            var response = await client.GetStringAsync(fullUrl).ConfigureAwait(false);
+            
+           // var responseString = await response.Result.ToString();
+
+            return response;
+        }
+    }
+}
